d6719a38ce39e1d5eacd76b9d3889931712ae437
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2021-04-22 20:56:56 +0600

    first commit

---
+++ src/main/java/spring/lab6/demo/controller/MemberController.java
@@ -0,0 +1,37 @@
+package spring.lab6.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import spring.lab6.demo.entity.Member;
+import spring.lab6.demo.service.implement.MemberService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+    @Autowired
+    private MemberService memberService;
+
+    // GET - 2
+    @GetMapping("")
+    public List<Member> getAllMembers() {
+        return memberService.getAll();
+    }
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World!";
+    }
+
+//    @GetMapping("")
+//    public Member getMember(@RequestParam("login") String login) {
+//        return memberService.getByLogin(login);
+//    }
+
+    // POST - 1
+    @PostMapping("/create")
+    public Member createNewMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+}
